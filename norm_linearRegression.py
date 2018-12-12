670745b379354fe73767ad73be53e5599a483b1d
yumi <yameihuang@qq.com>
2018-12-12 16:30:50 +0800

    from ubuntu

---
+++ norm_linearRegression.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import numpy as np
+
+# unsqueeze:在dim=?的维度添加一个维度
+x = torch.unsqueeze(torch.linspace(-5,5,200),dim=1)
+y = x.pow(2)+0.2*torch.rand(x.size())
+
+x, y = Variable(x),Variable(y)
+
+
+class NN(torch.nn.Module):
+    # 神经网络的累需要继承自torch.nn.Module,__init__和forward是自定义类的主要函数
+
+    # 对神经网络的模块进行声明
+    def __init__(self,input,hidden,output):
+        # 用于继承父类的初始化函数
+        super(NN,self).__init__()
+        self.hidden = torch.nn.Linear(input,hidden)
+        self.output = torch.nn.Linear(hidden,output)
+
+    # 搭建
+    def forward(self, x):
+        x = F.relu(self.hidden(x))
+        x = self.output(x)
+        return x
+net = NN(input=1,hidden=10,output=1)
+optimzer = torch.optim.SGD(net.parameters(),lr=0.01)
+criteroi = nn.MSELoss()
+print(net)
+# NN(
+#   (hidden): Linear(in_features=1, out_features=10, bias=True)
+#   (output): Linear(in_features=10, out_features=1, bias=True)
+# )
+for i in range(100):
+    predict = net(x)
+    loss = criteroi(predict,y)
+    optimzer.zero_grad()
+    loss.backward()
+    optimzer.step()
+    print("loss:"+str(loss.item()))
+plt.ion()
+for t in range(100):
+    plt.cla()
+    plt.scatter(x.data.numpy(),y.data.numpy())
+    plt.plot(x.data.numpy(),predict.data.numpy(),"r-",lw=5)
+    plt.text(0.5,0,"loss = %.4f"%(loss.item()))
+plt.ioff()
+plt.show()
